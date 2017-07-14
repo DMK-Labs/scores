a2f80df9aae20b6720fc11cd587039d8f93096fe
ryuryujun <o_o93@naver.com>
2017-07-14 18:25:45 +0900

    Add files via upload

---
+++ shopping/plotdata2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+
+k_time = pd.read_csv('gmarket_time.csv')
+k_time['datetime'] = k_time['datetime']/3600
+k_id = set(k_time['board_id'])
+
+start = k_time.iloc[0,3]
+end = k_time.iloc[-1,3]
+times = np.arange(start,end+0.25,0.25)
+to_time = lambda x: dt.datetime.fromtimestamp(x)
+x = np.array(map(to_time,times*3600))
+
+def line(i,x,sample):
+    a = (sample.iloc[i+1,1] - sample.iloc[i,1])/float((sample.iloc[i+1,3] - sample.iloc[i,3]))
+    b = sample.iloc[i,1] - a*sample.iloc[i,3]
+    y = a*x + b
+    return y
+
+def estimate(board_id,x):
+    # column numbers
+    ## 0: board_id, 1: clicks/y, 2: date(y-m-d h:m:s), 3:datetime/x 
+    sample = k_time[k_time.board_id==board_id]
+    age = 0
+    if x < sample.iloc[0,3]:
+        y = 0
+    elif x >= sample.iloc[-1,3]:
+        y = sample.iloc[-1,1]
+        age = x - sample.iloc[0,3]
+    else:
+        i = filter(lambda j: sample.iloc[j,3] <= x and sample.iloc[j+1,3] > x,
+                   range(len(sample)))
+        y = line(i[0],x,sample)
+        age = x - sample.iloc[0,3]
+    return y, age
+
+def score(board_id, time, gravity=1.8):
+    clicks,age = estimate(board_id,time)
+    return clicks / pow((age+2), gravity)
+
+
+scores = []
+m=0
+for board in k_id:
+    board_score = np.array(map(lambda z: score(board,z),times))
+    scores.append(board_score)
+    print m
+    m += 1
+y = sum(scores)
+
+key = 'G마켓'
+data = pd.DataFrame({'time_points':times,'scores':y,'x':x})
+data['keyword'] = key
+data.to_csv('gmarket_plot.csv',index=False,encoding='utf-8')
+
---
+++ shopping/scoreplot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+
+key1 = pd.read_csv('amazon_plot.csv')
+key1['x'] = key1['x'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+key2 = pd.read_csv('eleven_plot.csv')
+key2['x'] = key2['x'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+key3 = pd.read_csv('gmarket_plot.csv')
+key3['x'] = key3['x'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+key4 = pd.read_csv('wemap_plot.csv')
+key4['x'] = key4['x'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+#key5 = pd.read_csv('key5_plot.csv')
+#key5['x'] = key5['x'].map(lambda x: dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+
+x1,y1 = key1.x,key1.scores
+x2,y2 = key2.x,key2.scores
+x3,y3 = key3.x,key3.scores
+x4,y4 = key4.x,key4.scores
+#x5,y5 = key5.x,key5.scores
+
+plt.plot(x1,y1,label='Amazon')
+plt.plot(x2,y2,label='eleven')
+plt.plot(x3,y3,label='Gmarket')
+plt.plot(x4,y4,label='Wemap')
+#plt.plot(x5,y5,label='money')
+
+plt.title('scores by keywords')
+plt.legend(loc='upper left')
+plt.show()
